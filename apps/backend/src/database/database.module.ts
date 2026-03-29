520b1f699c20beaa30bc31c3453a1f68bb3c1d81
Vsevolod Zhabskyi <vsevolod_zhabskyi@epam.com>
2026-03-29 14:02:46 +0300

    chore(backend): setup drizzle-kit feat(backend): add auth schema integration

--- apps/backend/src/database/database.module.ts
+++ apps/backend/src/database/database.module.ts
@@ -3,6 +3,7 @@ import { DATABASE_CONNECTION } from './database-connection';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { Pool } from 'pg';
 import { drizzle } from 'drizzle-orm/node-postgres';
+import * as authSchema from '../auth/schema';
 
 @Module({
   imports: [ConfigModule],
@@ -15,7 +16,9 @@ import { drizzle } from 'drizzle-orm/node-postgres';
           connectionString: configService.getOrThrow<string>('DATABASE_URL'),
         });
 
-        return drizzle(pool);
+        return drizzle(pool, {
+          ...authSchema,
+        });
       },
     },
   ],
